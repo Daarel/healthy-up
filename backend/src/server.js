049f19d2a29befec3cc8cd34312eeec109878847
Fatillah <daarel.fatillah@outlook.com>
2026-05-26 19:00:46 +0700

    adjustments across all code bases

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,12 @@
-import express from "express";
-import { config } from "dotenv";
-import cors from "cors";
-import helmet from "helmet";
+import express from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
 
 // routes
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from './routes/authRoutes.js';
 
 config();
-
 const app = express();
 const PORT = process.env.PORT || 5001;
 
@@ -15,15 +14,16 @@ const PORT = process.env.PORT || 5001;
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API routes
-app.use("/api/v1/auth", authRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/healthyProfiles'); // need a change
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
