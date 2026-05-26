7685344be353ebff16b691421e4e29efa1246225
Fatillah <daarel.fatillah@outlook.com>
2026-05-26 18:59:29 +0700

    add healthProfiles route and controller

---
+++ backend/src/controllers/healthProfilesController.js
@@ -0,0 +1,7 @@
+import prisma from "../lib/prisma.js";
+
+/**
+ * @desc    Onboarding User
+ * @route   POST /api/v1/healthyProfiles/
+ * @access  Public
+ */
---
+++ backend/src/routes/healthProfilesRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+import { }
+
+const router = express.Router();
+
+router.post("/health-profiles");
+
+export default router;
