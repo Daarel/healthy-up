7a66648e0387c9166af2866610203f606b4b8fad
Fatillah <daarel.fatillah@outlook.com>
2026-05-27 09:29:25 +0700

    add getCaloriesSummary endpoint

--- backend/src/controllers/healthProfileController.js
+++ backend/src/controllers/healthProfileController.js
@@ -63,4 +63,82 @@ const createProfile = async (req, res) => {
   }
 };
 
-export { createProfile };
+/**
+ * * @desc    Get User Calories Data
+ * ! @route   GET /api/v1/health-profiles/caloriesSummary
+ * ? @access  Public
+ */
+const getCaloriesSummary = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const now = new Date();
+
+    const startOfToday = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+    );
+
+    const startOfWeek = new Date(startOfToday);
+    startOfWeek.setDate(startOfWeek.getDate() - 7);
+
+    const [dailyPhysical, weeklyPhysical, healthProfile] = await Promise.all([
+      // Kalori terbakar hari ini
+      prisma.mission.aggregate({
+        _sum: { caloriesImpact: true },
+        where: {
+          userId,
+          category: 'physical',
+          status: 'completed',
+          completedAt: { gte: startOfToday },
+        },
+      }),
+
+      // Kalori terbakar minggu ini
+      prisma.mission.aggregate({
+        _sum: { caloriesImpact: true },
+        where: {
+          userId,
+          category: 'physical',
+          status: 'completed',
+          completedAt: { gte: startOfWeek },
+        },
+      }),
+
+      prisma.healthProfile.findUnique({
+        where: { userId },
+        select: { goalWeight: true },
+      }),
+    ]);
+
+    const burnedToday = dailyPhysical._sum.caloriesImpact || 0;
+    const burnedWeekly = weeklyPhysical._sum.caloriesImpact || 0;
+
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        calories: {
+          burnedToday,
+          burnedWeekly,
+        },
+        profile: healthProfile,
+      },
+    });
+  } catch (err) {
+    console.error('Error fetching dashboard summary:', err);
+    return res.status(500).json({
+      status: 'error',
+      message: 'Gagal mengambil data dashboard',
+    });
+  }
+};
+
+/**
+ * * @desc    Add User Daily Weight Loss
+ * ! @route   GET /api/v1/health-profiles/getWeightLog
+ * ? @access  Public
+ */
+
+
+export { createProfile, getCaloriesSummary };
--- backend/src/routes/healthProfileRoutes.js
+++ backend/src/routes/healthProfileRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 
 import { createProfile } from '../controllers/healthProfileController';
+import { getCaloriesSummary } from '../controllers/healthProfileController';
+
 
 const router = express.Router();
 
 router.post('/', createProfile);
+router.get('/caloriesSummary', getCaloriesSummary);
 
 export default router;
